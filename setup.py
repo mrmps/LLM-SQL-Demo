238094be62985af66d83524d122e296adabe5f66
Michael Ryaboy <41216139+mrmps@users.noreply.github.com>
2024-10-04 05:24:59 +0000

    updates deps

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup, find_packages
 
-vllm = ["vllm", "triton==2.2.0"]
-
 setup(
-    name="llmsql",
-    version="0.1",
+    name='llmsql',
+    version='0.1',
+    description='LLM SQL Demo',
     packages=find_packages(),
-    install_requires = ["anthropic", "openai"],
-    extras_require = {
-        "vllm": vllm,
-    }
+    install_requires=[
+        'openai>=1.51.0',  # Update the version here
+        'jiter>=0.5.0',
+        'anthropic>=0.34.2',
+        'pydantic>=2.9.2',
+        'tqdm>=4.66.5',
+        'huggingface-hub>=0.25.1',
+        'tokenizers>=0.20.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Adjust based on your project's Python version
 )
